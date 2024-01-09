b0ca6d5e671cd55dd4b23aaa1b6ea8efd771429d
AmarDahiwalkar <amardahiwalkar312@gmail.com>
2024-01-09 18:20:49 +0530

    done

---
+++ Practice_DLL/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DLL d1 = new DLL();
+		d1.insert(10);
+		d1.insert(20);
+		d1.insert(30);
+		d1.insert(40);
+		d1.insert(50);
+		d1.display();
+
+	}
+
+}
